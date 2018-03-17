bbd9b913dce564f3392f0fd0f365a946f2ca87e8
Joshua Stewart <stewajos@dc13.cis.gvsu.edu>
2018-03-17 10:38:46 -0400

    at least im trying lol

--- cis241/prj2/products.c
+++ cis241/prj2/products.c
@@ -1,10 +1,18 @@
-include <stdio>
+#include <stdio>
+#include <string.h>
 struct product{
-	String name;
+	string name;
 	int quantity;
 	int price;
-	
+}
+int choice(int argc, char* argv[]){
+if(1 = argc){
+product *head = (product *)malloc(sizeof(product);
+d -> d = ‘n’; 
+head -> next = NULL;
+}
+if(2 = argc){
+string name = argv[];
 
-
-
+}
 }
