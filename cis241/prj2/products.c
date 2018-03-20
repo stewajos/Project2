5c82f849eac5681697d18ebe4b5853addff4e181
Daniel Schindler <schindld@dc18.cis.gvsu.edu>
2018-03-20 15:39:06 -0400

    Made methods for everything. Improved exsisting methods and added a few comments

--- cis241/prj2/products.c
+++ cis241/prj2/products.c
@@ -2,22 +2,116 @@
 #include <string.h>
 struct product{
 	char[20] name;
+	char[20] unit;
 	int quantity;
 	int price;
+	struct product* next;
 }
+//we should just do this in the main
 void createList(){
-	product *head = (product *)malloc(sizeof(product);
+	product *head = (product *)malloc(sizeof(product));
+	//where does this come from?
 	d -> d = ‘n’; 
 	head -> next = NULL;
 }
+
+//what? Why do we have this?
 void getInfo ( struct product * pItem)  {
 	fscanf ( "%d %s %d", &(pItem->price), pItem->name, pItem->quantity);
 }
-void deleteProuct(struct product *head){
-	if(head!=NULL){
-	deleteProduct(head->next);
-	free(head);
+
+
+
+//delete a product
+void deleteProduct(struct product *head, char* itemName){
+	while(head->next != NULL && 0 != strcmp(head->name, itemName)){
+		head = head->next;
+
+	}	
+	if(head->next == NULL){
+		return;
+		
+	}else{
+		struct product* temp = head->next->next;
+		free(head->next)
+		head->next = temp;
+	}	
+
+
 }
+
+//insert a product. Create the product in main and then just use this method
+void insert(struct product* newItem, struct product* head){
+	newItem->next = head->next;
+	head->next = newItem;
+	
+	
 }
+
+//Delete entire list
+void deleteAll(struct product* head){
+	printf("FIXME PLEASE! deleteAll method");
+
+}
+
+
+//search for a product by name
+struct product* search(struct product* head, char* itemName){
+	while(head->next != NULL && strcmp(head->name, itemName)){
+		head = head->next;
+	}
+	if(head->next == NULL){
+		struct product* fail = malloc(sizeof(product));
+		fail->name = "Not Found."
+		return fail;
+	}else{
+		return head;	
+	}
+}
+
+//Display products in list
+void display(struct product* head){
+	int count = 0;
+	while(head != NULL){
+		printf("Item %d:\n", count);
+		printf("Name: %s\n", head->name);
+		printf("Unit: %s\n", head->unit);
+		printf("Quantity: %d\n", head->quantity);
+		printf("Price: %d\n\n\n");
+		count++;
+		
+		head = head->next;
+		
+	}
+	
+	
+}
+
+//sell a product
+void sellProduct(struct product* head, char* itemName){
+	struct product* previous = head;
+	while(head->next != NULL && strcmp(head->name, itemName)){
+		head = head->next;
+		previous = head;
+	}
+	
+	if(head->next == NULL){
+		printf("Product not found for Deletion");
+		return NULL;
+		
+	}else{
+		head->quantity = head->quantity - 1;
+		if(head->quantity < 1){
+			struct product* temp = head->next->next;
+			previous->next = temp;
+			free(head);
+			
+		}
+	}
+	
+	
+}
+
+
 
 
