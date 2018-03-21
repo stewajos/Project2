1d168e38335af39f7ca602ba6a81c4d940b23a23
Joshua Stewart <stewajos@dc13.cis.gvsu.edu>
2018-03-21 14:35:10 -0400

    started the main - Josh

---
+++ cis241/prj2/main.c
@@ -0,0 +1,44 @@
+#include <stdio.h>
+#include <stdlib.h>
+int main( int argc, const char** argv){
+char option = 'q'; //variable to hold choices
+printf(" 1: create an empty list	2: insert a product\n 
+3: Delete a product	4: Delete entire list\n
+5: Search a product	6: Display products in the list\n
+7: Purchase a product	8: Sell a product\n
+9: Save to file 	0: Exit\n");
+while(option != 1 || option != 2 || option != 3 || option != 4 || option != 5 || option != 6 || option != 7 || option != 8 || option != 9 || option != 0){
+scanf("%c", option);
+
+if('1' == option){
+
+}
+else if('2' == option){
+
+}
+else if('3' == option){
+
+}
+else if('4' == option){
+deleteAll();
+}
+else if('5' == option){
+
+}
+else if('6' == option){
+
+}
+else if('7' == option){
+
+}
+else if('8' == option)
+
+}
+else if('9' == option)
+
+}
+else if('0' == option){
+exit(1);
+}
+}
+}
